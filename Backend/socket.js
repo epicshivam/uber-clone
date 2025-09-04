72fe1f848d2bbc67042ac119ef99952e0e09ceef
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-09-05 01:02:12 +0530

    socket io changes

--- Backend/socket.js
+++ Backend/socket.js
@@ -7,8 +7,9 @@ let io;
 function initializeSocket(server) {
     io = socketIo(server, {
         cors: {
-            origin: '*',
-            methods: ['GET', 'POST']
+            origin: "https://uber-clone-frontend-beta.vercel.app", // ✅ tumhara frontend ka deployed domain
+            methods: ["GET", "POST"],
+            credentials: true
         }
     });
 
